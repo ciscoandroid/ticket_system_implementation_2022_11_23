b3cd73688cb327b4295d42b9caa1f565734b8df4
Cisco_android <Cisco_android@DESKTOP-IJDNFK3>
2022-11-23 17:37:48 +0700

    Created Ticket System on CRUD

---
+++ Techbidia_ticket_system_CRUD/Techbidia_ticket_system_CRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Techbidia_ticket_system_CRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
